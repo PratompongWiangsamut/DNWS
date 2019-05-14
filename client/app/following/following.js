15ed3d82353f2d5e9148c19189cf1da6e128955c
VivereGrau <pratompongwiangsamut@gmail.com>
2019-05-14 23:29:02 +0700

    finish hw5

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -12,6 +12,22 @@ angular.module('followingList', ['ngRoute'])
 
       $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
         self.followings = response.data;
-      });
+        });
+
+        self.Following = function Following(followingname) {
+            const data = "followingname=" + encodeURIComponent(followingname);
+            $http.post('http://localhost:8080/twitterapi/following/', data, requestOptions).then(function (response) {
+                console.log(response);
+                alert("Start Follow " + encodeURIComponent(followingname))
+            });
+        }
+
+        self.Unfollow = function Unfollow(followingname) {
+            const data = "followingname=" + encodeURIComponent(followingname);
+            $http.delete('http://localhost:8080/twitterapi/following/?' + data, requestOptions).then(function (response) {
+                console.log(response);
+                alert("Unfollow " + encodeURIComponent(followingname))
+            });
+        }
     }]
 });
